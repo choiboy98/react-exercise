cabed79441be6cdae9ef5d4a2a294b0b23ea07fc
Daniel Choi <daniel.won.choi@gmail.com>
2018-09-17 02:29:27 -0500

    github shut down for some reason. So committing part 1-3 all together

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import { Instructions } from './components'
 import './styles/app.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  decrementCounter = () => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Hello World!</h1>
-        <Instructions />
+        <Instructions
+          shouldDisplayImage={true}
+          items={['apple', 'banana', 'clementine', 'dragonfruit', 'bl']}
+        />
+        <p>{this.state.counter}</p>
+        <button onClick={this.incrementCounter}>Increment</button>
+        <button onClick={this.decrementCounter}>Decrement</button>
       </div>
     )
   }
