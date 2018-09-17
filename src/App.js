581904a96a2aabd98dd9ed3f4de9c564af2e1f80
Daniel Choi <daniel.won.choi@gmail.com>
2018-09-17 16:21:41 -0500

    part 5 done

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     this.setState({ counter: this.state.counter - 1 })
   }
 
-  isNumeric = () => {
+  isNumeric = event => {
     var x = document.getElementById('initializeCounter').value
     if (!isNaN(parseFloat(x)) && isFinite(x)) {
       this.setState({ counter: x })
@@ -36,18 +36,29 @@ class App extends Component {
           shouldDisplayImage={true}
           items={['apple', 'banana', 'clementine', 'dragonfruit', 'bl']}
         />
-        <input
-          type="text"
-          id="initializeCounter"
-          placeholder="change your initial count"
+        <InitialCountForm
+          counter={this.state.counter}
+          changeCount={this.isNumeric}
         />
-        <button onClick={this.isNumeric}>Initialize Count</button>
-        <p>{this.state.counter}</p>
         <button onClick={this.incrementCounter}>Increment</button>
         <button onClick={this.decrementCounter}>Decrement</button>
+        <p>{this.state.counter}</p>
       </div>
     )
   }
+}
+
+const InitialCountForm = props => {
+  return (
+    <div>
+      <input
+        type="text"
+        id="initializeCounter"
+        placeholder="change your initial count"
+      />
+      <button onClick={props.changeCount}>Initialize Count</button>
+    </div>
+  )
 }
 
 export default App
